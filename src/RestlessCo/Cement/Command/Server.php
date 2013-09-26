289bb07ee3ecb4104dcadaebc77fea8d7634f0e9
Ciaran Downey <me@ciarand.me>
2013-09-26 01:31:54 -0700

    Update to use ReactPHP for the server command
    Now creates a server and runs the "runner". Also stops using
    environmental variables in favor of a cement.json file in the repo root.

--- src/RestlessCo/Cement/Command/Server.php
+++ src/RestlessCo/Cement/Command/Server.php
@@ -8,11 +8,14 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use RestlessCo\Cement\Core\Configuration;
-use RestlessCo\Cement\Build\Builder;
+use RestlessCo\Cement\Build\Job;
 
 class Server extends Command
 {
-    protected $config, $builder;
+    protected $input;
+    protected $output;
+    protected $config;
+    protected $job;
 
     protected function configure()
     {
@@ -25,46 +28,80 @@ class Server extends Command
                 'The directory to perform the CI on'
             )
             ->addOption(
-               'host',
-               // Can't use -h as a shortcut, help already uses it
-               null,
-               InputOption::VALUE_OPTIONAL,
-               'The hostname or ip of the host to bind to',
-               '0.0.0.0'
-           )
-            ->addOption(
-               'basedir',
-               'd',
-               InputOption::VALUE_OPTIONAL,
-               'Application base path for mounted instances',
-               '/'
-           )
-            ->addOption(
-               'port',
-               'p',
-               InputOption::VALUE_OPTIONAL,
-               'The port to listen on',
-               4567
+                'host',
+                // Can't use -h as a shortcut, help already uses it
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'The hostname or ip of the host to bind to',
+                '0.0.0.0'
             )
-        ;
+            ->addOption(
+                'basedir',
+                'd',
+                InputOption::VALUE_OPTIONAL,
+                'Application base path for mounted instances',
+                '/'
+            )
+            ->addOption(
+                'port',
+                'p',
+                InputOption::VALUE_OPTIONAL,
+                'The port to listen on',
+                4567
+            )
+            ;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $this->init($input);
-        $output->writeln("Starting the server!");
-
-        $output->writeln("Running tests:");
-        $this->builder->build();
-        $output->writeln("Build was " . ($this->builder->succeeded ? "successful" : "a failure"));
-        $output->writeln("Output:");
-        $output->writeln(str_repeat('=', 7));
-        $output->writeln($this->builder->output);
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ) {
+        $this->initializeVariables($input, $output);
+        $this->startServer();
     }
 
-    protected function init(InputInterface $input)
+    protected function initializeVariables($input, $output)
     {
+        $this->input = $input;
+        $this->output = $output;
         $this->config = new Configuration($input->getArgument('repo'));
-        $this->builder = new Builder($this->config['RUNNER']);
+        $this->output->writeln("Starting server with runner command:");
+        $this->output->writeln($this->config['runner']);
+        $this->job = new Job($this->config['runner']);
+    }
+
+    /**
+     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
+     */
+    protected function startServer()
+    {
+        $this->host = $this->input->getOption('host');
+        $this->baseDir = $this->input->getOption('basedir');
+        $this->port = $this->input->getOption('port');
+
+        $app = function ($request, $response) {
+            // Set the status code
+            $response->writeHead(200, array('Content-Type' => 'text/plain'));
+            // Write that we're testing
+            $response->write("Beginning test...");
+            $this->job->run();
+            if (!$this->job->exitCode) {
+                $response->write("Success!" . PHP_EOL);
+            } else {
+                $response->write("Failure!" . PHP_EOL);
+            }
+            $response->end($this->job->getOutput());
+            $this->output->writeln("<info>Job completed</info>");
+        };
+
+        $loop = \React\EventLoop\Factory::create();
+        $socket = new \React\Socket\Server($loop);
+        $http = new \React\Http\Server($socket, $loop);
+
+        $http->on('request', $app);
+        echo "Server running at http://127.0.0.1:{$this->port}\n";
+
+        $socket->listen($this->port);
+        $loop->run();
     }
 }
--- src/RestlessCo/Cement/Core/Configuration.php
+++ src/RestlessCo/Cement/Core/Configuration.php
@@ -2,42 +2,45 @@
 
 namespace RestlessCo\Cement\Core;
 
-class Configuration implements \ArrayAccess {
+class Configuration implements \ArrayAccess
+{
     protected $config = [];
 
     public function __construct($dir)
     {
-        \Dotenv::load($dir, '.cement');
-        $this->requireConfigParameters(['RUNNER', 'BRANCH']);
+        $this->config = json_decode(
+            file_get_contents($dir . '/cement.json'),
+            true
+        );
+        $this->requireConfigParameters(['runner', 'branch']);
     }
 
     protected function requireConfigParameters($params)
     {
-        \Dotenv::required($params);
+        return empty(array_diff(array_flip($params), $this->config));
     }
 
     // abstract public boolean offsetExists ( mixed $offset )
     public function offsetExists($offset)
     {
-        return (bool) getenv($offset);
+        return isset($this->config[$offset]);
     }
 
     // abstract public mixed offsetGet ( mixed $offset )
     public function offsetGet($offset)
     {
-        return getenv($offset);
+        return $this->config[$offset];
     }
 
     // abstract public void offsetSet ( mixed $offset , mixed $value )
     public function offsetSet($offset, $value)
     {
-        putenv("$offset=$value");
+        $this->config[$offset] = $value;
     }
 
     // abstract public void offsetUnset ( mixed $offset )
     public function offsetUnset($offset)
     {
-        putenv($offset);
+        unset($this->config[$offset]);
     }
-
 }
