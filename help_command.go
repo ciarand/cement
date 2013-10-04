7c8e453c301457f67b10d81f4a8f5c088c5b6b47
Ciaran Downey <me@ciarand.me>
2013-10-04 10:47:03 -0700

    Add (slightly) more helpful help command

--- help_command.go
+++ help_command.go
@@ -5,5 +5,11 @@ import (
 )
 
 func helpCommand() {
-	fmt.Println("Help!")
+	fmt.Println(`
+Cement -- A multipurpose build server / CLI written in Go
+
+Commands:
+    build               Builds the current project
+    init                Generates a .cement file for the current project
+	`)
 }
