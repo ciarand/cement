6eeba493dc1b3634ed6b9bb1b129248795f9973c
Ciaran Downey <me@ciarand.me>
2013-10-04 12:34:51 -0700

    Make previous test pass by adjusting help output

--- help_command.go
+++ help_command.go
@@ -9,7 +9,6 @@ func helpCommand() {
 Cement -- A multipurpose build server / CLI written in Go
 
 Commands:
-    build               Builds the current project
-    init                Generates a .cement file for the current project
-	`)
+	build			Builds the current project
+	init			Generates a .cement file for the current project`)
 }
