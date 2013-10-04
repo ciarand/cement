65b2a9357d2bea2fa388a112f4664498e77ac8a5
Ciaran Downey <me@ciarand.me>
2013-10-04 12:33:59 -0700

    Add a simple help command test

---
+++ help_command_test.go
@@ -0,0 +1,13 @@
+package main
+
+import ()
+
+func ExampleHelpCommand() {
+	helpCommand()
+	// Output:
+	// Cement -- A multipurpose build server / CLI written in Go
+	//
+	// Commands:
+	// 	build			Builds the current project
+	// 	init			Generates a .cement file for the current project
+}
